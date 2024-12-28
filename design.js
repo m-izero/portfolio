52f4eb5ad88e07f423b751a5f8b142b98e3401ef
m-izero <mizerohirwaelyse@gmail.com>
2024-12-28 15:54:09 +0530

    quick change

---
+++ design.js
@@ -0,0 +1,10 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    const exampleText = ["web designer", "frontend developer", "ui/ux designer", "software developer"];
+    const exampleTyping = new AutoTyping('.example-selector', exampleText, {
+        typeSpeed: 50,
+        deleteSpeed: 50,
+        waitBeforeDelete: 2000,
+        waitBetweenWords: 500,
+    });
+    exampleTyping.start()
+});*/
