954d6924adfed54f0e3fb82ee1f0d1325e08711b
m-izero <mizerohirwaelyse@gmail.com>
2025-01-04 04:02:09 +0530

    anather change

--- design.js
+++ design.js
@@ -7,4 +7,15 @@
         waitBetweenWords: 500,
     });
     exampleTyping.start()
-});*/
+});
+var typed = new Typed(".auto-type",{
+    Strings : ["ui/ux designer","software developer","fullstack developer","networking"],
+    typespeed : 400,
+    backspeed : 2000,
+    loop : true
+  })
+  typed.start();*/
+
+  // scroll reveal
+
+  
